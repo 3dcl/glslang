e206e22ab39de17143c1acab28d64c4611146e20
Christophe Riccio <christophe.riccio@unity3d.com>
2013-09-27 12:52:51 +0200

    Added copy to install dictory of the glslangValidator. Renamed Linux file directory to Posix

--- glslang/OSDependent/osinclude.h
+++ glslang/OSDependent/osinclude.h
@@ -3,10 +3,8 @@
 
 #ifdef _WIN32
 #	include "../OSDependent/windows/osinclude.h"
-//#elif __APPLE__
-//#	include "../OSDependent/macos/osinclude.h"
 #else
-#	include "../OSDependent/linux/osinclude.h"
+#	include "../OSDependent/posix/osinclude.h"
 #endif
 
 #endif//OSDEPENDENT_OSINCLUDE_INCLUDED
